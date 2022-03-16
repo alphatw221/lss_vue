c4d7aaeeeaf5c18010ea3b1ce4911619e08d4319
asdf50003206 <asdf500032061@gmail.com>
2022-03-16 17:22:28 +0800

    D table

---
+++ src/components/table/DynamicTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <table>
+        <thead>
+                <tr >
+                    <th v-for="column in columns" v-bind:key="column.key"></th>
+                </tr>
+        </thead>
+        <tbody v-for="item in listItems" v-bind:key="item.id" >
+            <tr v-for="column in columns" v-bind:key="column.key">
+                <td v-html="item[column.key]">
+                </td>
+            </tr>
+        </tbody>
+</table>
+
+</template>
+
+<script>
+// import { axiosInstance } from '@/libs/axiosClient'
+
+export default {
+    props:{
+        tableName:String,
+        requestUrl:String,
+        columns:Array
+    },
+    data(){
+        return{
+            listItems:[
+                {
+                    "id":1,
+                    "name":"test"
+                },
+                {
+                    "id":2,
+                    "name":"test2"
+                },
+            ],
+        }
+    },
+    mounted(){
+        this.eventBus.on("searchTable", payload => {
+            this.listItems=[
+                {
+                    "id":3,
+                    "name":"test3"
+                },
+                {
+                    "id":4,
+                    "name":"test4"
+                },
+            ]
+        });
+        
+    },
+    unmounted(){
+        this.eventBus.off("searchTable");
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
