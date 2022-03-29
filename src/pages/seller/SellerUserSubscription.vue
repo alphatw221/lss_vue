13d4ebace5a720c61183338deb4ff9336813025c
root <root@LAPTOP-5LMCTULN.localdomain>
2022-03-29 14:44:54 +0800

    update

--- src/pages/seller/SellerUserSubscription.vue
+++ src/pages/seller/SellerUserSubscription.vue
@@ -7,15 +7,20 @@
         <v-main>
             <SearchBar 
                 :searchColumns="searchColumns"
-            ></SearchBar>
+            />
             <v-divider></v-divider>
-            <v-table height="500">
+            <SubTable
+                :tableName="'User Subscription Table'"
+                :requestUrl="'/api/user-subscription/dealer_search_list'"
+                :columns="tableColumns"
+            />
+            <!-- <v-table height="500">
                 <thead>
-                <tr>
-                    <th v-for="column in tableColumns" :key="column.key" style="font-size:1rem;">
-                        {{column.name}}
-                    </th>
-                </tr>
+                    <tr>
+                        <th v-for="column in tableColumns" :key="column.key" style="font-size:1rem;">
+                            {{column.name}}
+                        </th>
+                    </tr>
                 </thead>
                 <tbody v-for="item in listItems" :key="item.id">
                     <tr @click="showItem=item.id">
@@ -27,20 +32,21 @@
                     <tr v-if="item.id == showItem">aa</tr>
                     <tr v-if="item.id == showItem">aa</tr>
                     <tr v-if="item.id == showItem">aa</tr>
-                    
                 </tbody>
-            </v-table>
+            </v-table> -->
         </v-main>
     </v-app>
 </template>
 <script>
 import PageBar from '@/components/bar/PageBar.vue';
-import SearchBar from "@/components/bar/SearchBar.vue";
+import SearchBar from '@/components/bar/SearchBar.vue';
+import SubTable from '@/components/table/SubscriptionTable.vue';
 
 export default {
     components: {
         PageBar,
-        SearchBar
+        SearchBar,
+        SubTable
    },
     data() {
         return {
@@ -49,10 +55,10 @@ export default {
             tableColumns:[
                 {key:'id',  name:'ID'},
                 {key:'country', name:'Country'},
-                {key:'plan', name:'Plan'},
+                {key:'type', name:'Plan'},
                 {key:'trial_period', name:'Trial Period'},
-                {key:'contract_period', name:'Contract Period'},
-                {key:'company', name:'Company Brand'},
+                {key:'expired_at', name:'Contract Period'},
+                {key:'name', name:'Company Brand'},
                 {key:'std', name:'Sales Till Dates(STD)'},
                 {key:'person', name:'Person in Charge'},
                 {key:'phone', name:'Phone'},
@@ -61,7 +67,6 @@ export default {
                 {'id': '1', 'country': 'Malaysia', 'plan': 'Trial', 'trial_period': '1 Month', 'contract_period': '20220203', 'company': 'OSC', 'std': '12000', 'person': 'Pooop', 'phone': '0000000'},
                 {'id': '2', 'country': 'Malaysia', 'plan': 'Trial', 'trial_period': '1 Month', 'contract_period': '20220203', 'company': 'OSC', 'std': '12000', 'person': 'Pooop', 'phone': '0000000'}
             ],
-            info_show: false,
             showItem: null
         }
     },   
