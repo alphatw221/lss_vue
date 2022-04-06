8281bfc52e34ce571f698ca883d283ff819ec514
root <root@LAPTOP-5LMCTULN.localdomain>
2022-04-06 10:16:29 +0800

    update dashboard

--- src/pages/seller/SellerDashBoardPage.vue
+++ src/pages/seller/SellerDashBoardPage.vue
@@ -45,6 +45,8 @@
 import AmountCard from '@/components/card/AmountCard.vue';
 import PageBar from '@/components/bar/PageBar.vue';
 import LineChart from '@/components/chart/LineChart.vue'
+import { axiosInstanceWithBearer } from "@/libs/axiosClient";
+import { dealer_dashboard, dealer_campaign } from '@/api/dealer';
 
 export default {
    components: {
@@ -55,10 +57,10 @@ export default {
    data() {
       return {
          pageName: 'DashBoard',
-         transactionItems: { title: 6000, subtitle: 'Total Transactions', img: require("@/assets/lss-console-icon/dollar.png"), style:'background: linear-gradient(45deg, #43A047, #1de9b6)' },
-         contractItems: { title: 900, subtitle: 'Contracts due soon', img: require("@/assets/lss-console-icon/contract.png"), style:'background: linear-gradient(45deg, #8e24aa, #ff6e40)' },
-         buyerItems: { title: 900, subtitle: 'Total Buyers', img: require("@/assets/lss-console-icon/people.png"), style:'background: linear-gradient(45deg, #3949ab, #4fc3f7)' },
-         campaignItems: { title: 900, subtitle: 'Total Campaign', img: require("@/assets/lss-console-icon/youtube.png"), style:'background: linear-gradient(45deg, #bf360c, #f57c00)' },
+         transactionItems: { title: 0, subtitle: 'Total Transactions', img: require("@/assets/lss-console-icon/dollar.png"), style:'background: linear-gradient(45deg, #43A047, #1de9b6)' },
+         contractItems: { title: 0, subtitle: 'Contracts due soon', img: require("@/assets/lss-console-icon/contract.png"), style:'background: linear-gradient(45deg, #8e24aa, #ff6e40)' },
+         buyerItems: { title: 0, subtitle: 'Total Buyers', img: require("@/assets/lss-console-icon/people.png"), style:'background: linear-gradient(45deg, #3949ab, #4fc3f7)' },
+         campaignItems: { title: 0, subtitle: 'Total Campaign', img: require("@/assets/lss-console-icon/youtube.png"), style:'background: linear-gradient(45deg, #bf360c, #f57c00)' },
          lineChart: {
             data: {
                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
@@ -68,23 +70,100 @@ export default {
                      data: [65, 59, 80, 81, 56, 55, 40],
                      fill: false,
                      borderColor: 'rgb(75, 192, 192)',
-                     tension: 0.1
+                     tension: 0.3
                   },
                   {
                      label: 'My First Dataset',
                      data: [15, 29, 30, 21, 26, 15, 10],
                      fill: false,
                      borderColor: 'rgb(75, 100, 192)',
-                     tension: 0.1
+                     tension: 0.3
                   },
                ]
             }
          }
       }
    },
+   computed: {
+      // lineChart: () => {
+      //    let lineChart = {
+      //       data: {
+      //          labels: [],
+      //          datasets: []
+      //       }
+      //    }
+
+      //    dealer_campaign()
+      //    .then(
+      //       response=>{
+      //          lineChart.data.labels = response.data.labels
+      //          lineChart.data.datasets = response.data.data_sets
+      //          console.log(this.lineChart)
+      //       }
+      //    ).catch(
+      //       error=>{
+      //          console.log(error);
+      //       }
+      //    )
+         
+      //    return lineChart
+      // }
+   },
+   setup() {
+      // this.getDashboardCard()
+   },
    mounted() {
-      console.log('poooooooooooooop')
+      this.getDashboardCard()
+      // this.getCampaingLineChart()
+
       console.log(this.$store.state.role)
+   },
+   methods: {
+      getDashboardCard() {
+         dealer_dashboard()
+         .then(
+            response=>{
+               this.transactionItems.title = this.addDecimalPoints(response.data.transaction.toString());
+               this.contractItems.title = this.addDecimalPoints(response.data.contract_due_soon.toString());
+               this.buyerItems.title = this.addDecimalPoints(response.data.buyers.toString());
+               this.campaignItems.title = this.addDecimalPoints(response.data.campaigns.toString());
+            }
+         ).catch(
+            error=>{
+               console.log(error);
+            }
+         )
+      },
+      getCampaingLineChart() {
+         dealer_campaign()
+         .then(
+            response=>{
+               this.lineChart.data.labels = response.data.labels
+               this.lineChart.data.datasets = response.data.data_sets
+               console.log(this.lineChart)
+            }
+         ).catch(
+            error=>{
+               console.log(error);
+            }
+         )
+      },
+      addDecimalPoints(val) {
+         val = val.replace(/\D/g, '');
+         var inputValue = val.replace('.', '').split('').reverse().join(''); // reverse
+         var newValue = '';
+         for (var i = 0; i < inputValue.length; i++) {
+               if (i % 3 == 0) {
+                  newValue += ',';
+               }
+               newValue += inputValue[i];
+         }
+         val = newValue.split("").reverse().join("");
+         if (val.at(-1) == ',') {
+            val = val.slice(0, -1)
+         }
+         return val
+      }
    },
 }
 </script>
